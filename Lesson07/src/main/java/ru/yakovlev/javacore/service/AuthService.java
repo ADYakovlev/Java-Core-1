2f6fa85f0f6b6502cd28036661c14e50ceb5929d
ADYakovlev <ay@skb.ru>
2018-07-26 15:50:23 +0500

    client, server from zip lesson

---
+++ Lesson07/src/main/java/ru/yakovlev/javacore/service/AuthService.java
@@ -0,0 +1,11 @@
+package ru.volnenko.javacore.lesson7.server.service;
+
+public interface AuthService {
+
+    void start();
+
+    String getNickByLoginPass(String login, String pass);
+
+    void stop();
+
+}
